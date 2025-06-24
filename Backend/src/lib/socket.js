a266e1c4360a92771402c5ece9a7d330c27a307b
isahibjit <sahibjitsinghramgharia@gmail.com>
2025-06-24 13:01:35 +0530

    fix: socket

--- Backend/src/lib/socket.js
+++ Backend/src/lib/socket.js
@@ -9,7 +9,8 @@ const server = createServer(app)
 const io = new Server(server, {
     cors: {
         origin: [process.env.FRONTEND_URL],
-        credentials : true,
+        methods: ["GET", "POST"],
+        credentials: true,
     }
 })
 const userSocketMap = {}
